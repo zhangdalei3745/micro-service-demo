dbcf428c5c8fab2591ecac538d5151811305ca75
zhangdalei1 <zhangdalei1@jd.com>
2022-03-16 00:07:46 +0800

    update dockerfile

--- consul/spring-cloud-consul-demo/src/main/bin/sgm.sh
+++ consul/spring-cloud-consul-demo/src/main/bin/sgm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ $SGM_ENABLE ]; then
-  curl -s "http://$SGM_CONSOLE_SERVER/local/probe/java/script?app=$APP_NAME-$APP_GROUP" -o /tmp/sgm_java.sh && source /tmp/sgm_java.sh && java $SGM_OPTS -Dserver.port=80 -jar /app/jmsf-consumer.jar
+  curl -s "http://$SGM_CONSOLE_SERVER/local/probe/java/script?app=$APP_NAME-$APP_GROUP" -o /tmp/sgm_java.sh && source /tmp/sgm_java.sh && java $SGM_OPTS -Dserver.port=80 -Dspring.cloud.loadbalancer.enabled=${ENABLE_REGISTRY} -Dspring.cloud.consul.discovery.enable=${ENABLE_REGISTRY} -jar /app/jmsf-consumer.jar
 else
-  java  -Dserver.port=80 -jar /app/jmsf-consumer.jar
+  java  -Dserver.port=80 -Dspring.cloud.loadbalancer.enabled=$ENABLE_REGISTRY -Dspring.cloud.consul.discovery.enable=$ENABLE_REGISTRY -jar /app/jmsf-consumer.jar
 fi
 
